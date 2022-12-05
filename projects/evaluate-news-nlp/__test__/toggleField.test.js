50bea88764b0df45719a55305b9c8c0c8f391ffa
Richard Bailey <richwb103@gmail.com>
2022-12-06 00:50:51 +1100

    6/12

---
+++ projects/evaluate-news-nlp/__test__/toggleField.test.js
@@ -0,0 +1,9 @@
+import { toggleFieldfn } from '../src/client/js/toggleField'
+
+describe("testing toggleField fn", () =>{
+    test("testing if toggleField fn checks attribute ", () => {
+        
+
+        expect(toggleFieldfn).toBeDefined();
+    } )
+} )
